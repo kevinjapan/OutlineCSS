d9a967f05fae6a1127ae9149ebd7a3b0dfdf7958
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-05-24 12:53:07 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!--
+don't do a whole lot - but what you do, do well - by design
+
+build things and build my own stuff - build software
+build something very well
+simplicity and clarity
+simple, small, focus, clarity simplicity
+
+focus - take one small thing at a time - start small, make it smaller
+scale it down and see what you can do - cut your ambitions in half
+cut schedule / people / time / decisions  in half
+version 1 should be cut in half - very few things you actually need
+
+build things for yourself first - you know the problem, you'll find the solution
+
+make things simple - do less
+you don't compete by doing more
+it's too easy - people will copy? - but they don't
+they will complicate it
+most people's problems are pretty simple
+get the basics right - pay attention to them
+most big companies don't make simple things for reasons
+eg selling new versions/ adding new features .. etc
+
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OutlineCSS</title>
+
+    <link rel="stylesheet" type="text/css" href="outlinecss.css">
+
+    <style>
+        ul {list-style:none;padding:.5rem; width:100%;}
+    </style>
+</head>
+<body>
+
+   <main>
+
+         <h1>outline-css</h1>
+
+         <h5  class="lightgray font-100">
+               CSS kickstart your web project
+         </h5>
+
+         <section>
+            <a href="themes/index.php">themes</a>
+         </section>
+      
+   </main>
+</body>
+</html>
---
+++ themes/education/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>OutlineCSS</title>
+    <link rel="stylesheet" type="text/css" href="../../outlinecss.css">
+    <link rel="stylesheet" type="text/css" href="education-outline-custom-props.css">
+    <link rel="stylesheet" type="text/css" href="education-outline-utilities.css">
+
+</head>
+<body>
+
+
+
+   <main>
+
+            <h1><span class="grey_text">theme</span><span>education</span></h1>
+            <h5  class="lightgray font-100">
+                simple bottom-up CSS kickstart for your web projects
+            </h5>
+            <section>
+
+                <!-- ---------------------------------------------
+                    themes 
+                    --------------------------------------------- -->
+                
+                <a href="pages/home.html">view site</a>
+                <a href="education-outline-styleguide.html">styleguide</a>
+                <a href="education-outline-design-elements.html">design elements</a>
+                <!-- <a href="music/music-outlinecss.html">music</a>
+                <a href="web-dev-agency/web-agency-outlinecss.html">web design agency</a> -->
+
+            </section>
+   </main>
+</body>
+</html>
